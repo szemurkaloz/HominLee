e7c16c4a72e43b9fe76793ed3b100ff67d3b920a
Homin Lee <homin.lee@suapapa.net>
2015-12-01 19:49:24 +0900

    Follow 1.The Hero Editor

--- week2/angular2-tour-of-heroes/src/app/app.ts
+++ week2/angular2-tour-of-heroes/src/app/app.ts
@@ -1,9 +1,29 @@
-import {bootstrap, Component} from 'angular2/angular2';
+import {bootstrap, Component, FORM_DIRECTIVES} from 'angular2/angular2';
+
+class Hero {
+    id: number;
+    name: string;
+}
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: `
+    <h1>{{title}}</h1>
+    <h2>{{hero.name}} detailes!</h2>
+    <div><label>id: </label>{{hero.id}}</div>
+    <div>
+      <label>name: </label>
+      <div><input [(ng-model)]="hero.name" placeholder="name"></div>
+    </div>
+    `,
+    directives: [FORM_DIRECTIVES]
 })
-class AppComponent { }
+class AppComponent {
+    public title = "Tour of Heroes";
+    public hero: Hero = {
+        id: 1,
+        name: 'Windstorm'
+    };
+}
 
 bootstrap(AppComponent);
